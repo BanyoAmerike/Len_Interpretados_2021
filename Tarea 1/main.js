fecff10db5d69145abf81958d2eb5591b7beb2e9
Fernando747 <fernandojcr5@gmail.com>
2021-08-24 08:56:57 -0500

    Ejercicio Bisiesto

---
+++ Tarea 1/main.js
@@ -0,0 +1,16 @@
+function leap(){
+    let input_value = document.getElementById("year").value;
+    if (input_value%4 == 0){
+        if(input_value%100 == 0){
+            if(input_value%400 == 0){
+                alert("Es bisiesto");
+            } else {
+                alert ("No es bisiesto");
+            }
+        } else {
+            alert ("Es bisiesto");
+        }       
+    } else{
+        alert ("No es bisiesto");
+    }
+}
