9dddeceebc93c4e7e079857de14ac229dbcb7d27
Daniel <cdmx1963@amerike.edu.mx>
2021-08-25 08:49:04 -0500

    25/08
    :D

--- Ejercicio 1/js/main.js
+++ Ejercicio 1/js/main.js
@@ -8,5 +8,15 @@ function leap()
     {
         alert("Este es el valor del input" + inpput_value);
     }
+}
+function key_validation(event)
+{
+    let dato = parseInt(event.key);
+    if (!Number.isInteger(dato)) {
+        alert("Escribe puros números, gato");
+    }
+    else
+    {
 
+    }
 }
