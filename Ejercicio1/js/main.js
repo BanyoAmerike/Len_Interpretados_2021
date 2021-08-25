b2cd2141972ae8b1762a27024080a14ac043d5dd
Diana Ramos <diana_ramos_g@hotmail.com>
2021-08-25 09:03:59 -0500

    UpdateEjercicio1

--- Ejercicio1/js/main.js
+++ Ejercicio1/js/main.js
@@ -6,9 +6,33 @@ function leap()
 {
     let input_value = document.getElementById("year").value;
     let ver = input_value % 4;
-    if(ver == 0) alert('Este año ' + input_value + " es biciesto");
-
-    
+    if(ver == 0) alert('Este año ' + input_value + " es biciesto");  
 }
+
+function key_validation(event)
+    {
+        //Respaldar info antes de que se agregue la tecla
+        var inputBackup = document.getElementById("year").value;
+
+
+
+
+        //Se hace validación si es tecla num
+        let dato = parseInt(event.key);
+
+        if(!Number.isInteger(dato))
+        {
+        //Se debe regresar la informacion respaldada
+            alert("Escribe unicamente numeros por favor " + event.key); 
+            let inputYear = document.getElementById("year");
+            inputYear.value = inputBackup;
+        }
+        else
+        {
+            //se regresa el respaldo con el nuevo dato 
+
+        } 
+    }
+
 
 //Referencia al documento
