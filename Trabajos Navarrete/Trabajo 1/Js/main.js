7673473c448aa23c9ba8b79fda2a9c779fe30aa4
Daniel Navarrete <daniel_navarrete_05@hotmail.com>
2021-08-25 08:55:30 -0500

    Ejercicio 1 cambios

--- Trabajos Navarrete/Trabajo 1/Js/main.js
+++ Trabajos Navarrete/Trabajo 1/Js/main.js
@@ -2,10 +2,25 @@
 
 function leap(){
 
-    let imput_value = document.getElementById("year").value;
+    let input_value = document.getElementById("year").value;
     let ver = input_value % 4;
     if (ver == 0)
     {
         alert("El año: " + input_value + " es biciesto");
     }
+}
+
+function Key_validation(event){
+
+    var input_backup = document.getElementById("year").value;
+
+    let dato = parseInt(event.keyCode);
+
+    if(!Number.isInteger(dato))
+    {
+        alert("Escriba unicamente numeros");
+        let input_year = document.getElementById("year");
+        input_year.value = input_backup;
+    }
+    
 }
