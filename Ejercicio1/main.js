e0f4192d34fdd7dba53179f9978c0cf3d3f7ff15
Fernando747 <fernandojcr5@gmail.com>
2021-08-25 08:51:11 -0500

    Ejercicio 1 actualizado

--- Ejercicio1/main.js
+++ Ejercicio1/main.js
@@ -14,3 +14,22 @@ function leap(){
         alert ("No es bisiesto");
     }
 }
+
+function key_validation(event){
+
+    //Respaldar informacion 
+var input_backup = document.getElementById("year").value;
+
+   let dato = parseInt (event.key);
+
+
+   if (!Number.isInteger(dato)){
+
+    //Se debe regresar la informacion respaldada
+       alert("Escribe solo numeros por favor");
+       let input_year = document.getElementById("year");
+       input_year.value = input_backup;
+   } else {
+    //Se regresa el respaldo con el nuevo dato 
+   }
+}
