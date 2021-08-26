0fd134b2ce14ddb830de17cdfbdb3b10093a1a57
JuanEstrada01 <79321459+JuanEstrada01@users.noreply.github.com>
2021-08-25 23:30:43 -0500

    a
    25/08

--- Tarea 1/main.js
+++ Tarea 1/main.js
@@ -1,9 +1,16 @@
 function leap(){
-    let imput_value = document.getElementById("year").value; 
- var ver = 2020 % 4;
- if (ver == 0){
-     console.log("Es biciesto");
- }
- alert ("El año" +  imput_value + "es biciesto") ;
- 
- }
+    let input_value = document.getElementById("year").value;
+    if (input_value%4 == 0){
+        if(input_value%100 == 0){
+            if(input_value%400 == 0){
+                alert("Es bisiesto");
+            } else {
+                alert ("No es bisiesto");
+            }
+        } else {
+            alert ("Es bisiesto");
+        }
+    } else{
+        alert ("No es bisiesto");
+    }
+}
