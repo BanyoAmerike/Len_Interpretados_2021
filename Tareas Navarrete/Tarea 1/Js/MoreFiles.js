6bbb7ae3a905902c97a55cb1b4a67b37ff45e7e0
Daniel Navarrete <daniel_navarrete_05@hotmail.com>
2021-08-29 01:13:07 -0500

    Adelanto de tarea
    Harcodeada y sin diseño

---
+++ Tareas Navarrete/Tarea 1/Js/MoreFiles.js
@@ -0,0 +1,260 @@
+function addFiles(){
+
+        var InputValue = document.getElementById("sides").value;
+
+        sideOne = document.getElementById("one");
+        sideTwo = document.getElementById("two");
+        sideThree = document.getElementById("three");
+        sideFour = document.getElementById("four");
+        sideFive = document.getElementById("five");
+        sideSix = document.getElementById("six");
+        sideSeven = document.getElementById("seven");
+        sideEight = document.getElementById("eight");
+        sideNine = document.getElementById("nine");
+        sideTen = document.getElementById("ten");
+        
+        theButton = document.getElementById("nextButton");
+
+        theFigure = document.getElementById("showFigure");
+        thePerimeter = document.getElementById("showPerimeter");
+
+        switch (InputValue)
+        {
+                case "3": 
+                                sideOne.innerHTML = '<input id = "One" type = "number" >';
+                                sideTwo.innerHTML = '<input id = "Two" type = "number" >';
+                                sideThree.innerHTML = '<input id = "Three" type = "number" >';
+                                sideFour.innerHTML = '<div id = "Four"> </div>';
+                                sideFive.innerHTML = '<div id = "Five"> </div>';
+                                sideSix.innerHTML = '<div id = "Six"> </div>';
+                                sideSeven.innerHTML = '<div id = "Seven"> </div>';
+                                sideEight.innerHTML = '<div id = "Eight"> </div>';
+                                sideNine.innerHTML = '<div id = "Nine"> </div>';
+                                sideTen.innerHTML = '<div id = "Ten"> </div>';
+
+                                theButton.innerHTML = '<button type = "button" onclick = "typeOfFigure()" >Resultado</button>';
+                                theFigure.innerHTML = "";
+                                thePerimeter.innerHTML = "";
+                break;
+
+                case "4": 
+                                sideOne.innerHTML = '<input id = "One" type = "number" >';
+                                sideTwo.innerHTML = '<input id = "Two" type = "number" >';
+                                sideThree.innerHTML = '<input id = "Three" type = "number" >';
+                                sideFour.innerHTML = '<input id = "Four" type = "number" >';
+                                sideFive.innerHTML = '<div id = "Five"> </div>';
+                                sideSix.innerHTML = '<div id = "Six"> </div>';
+                                sideSeven.innerHTML = '<div id = "Seven"> </div>';
+                                sideEight.innerHTML = '<div id = "Eight"> </div>';
+                                sideNine.innerHTML = '<div id = "Nine"> </div>';
+                                sideTen.innerHTML = '<div id = "Ten"> </div>';
+
+                                theButton.innerHTML = '<button type = "button" onclick = "typeOfFigure()" >Resultado</button>';
+                                theFigure.innerHTML = "";
+                                thePerimeter.innerHTML = "";
+                break;
+
+                case "5": 
+                                sideOne.innerHTML = '<input id = "One" type = "number" >';
+                                sideTwo.innerHTML = '<input id = "Two" type = "number" >';
+                                sideThree.innerHTML = '<input id = "Three" type = "number" >';
+                                sideFour.innerHTML = '<input id = "Four" type = "number" >';
+                                sideFive.innerHTML = '<input id = "Five" type = "number" >';
+                                sideSix.innerHTML = '<div id = "Six"> </div>';
+                                sideSeven.innerHTML = '<div id = "Seven"> </div>';
+                                sideEight.innerHTML = '<div id = "Eight"> </div>';
+                                sideNine.innerHTML = '<div id = "Nine"> </div>';
+                                sideTen.innerHTML = '<div id = "Ten"> </div>';
+
+                                theButton.innerHTML = '<button type = "button" onclick = "typeOfFigure()" >Resultado</button>';
+                                theFigure.innerHTML = "";
+                                thePerimeter.innerHTML = "";
+                break;
+
+                case "6": 
+                                sideOne.innerHTML = '<input id = "One" type = "number" >';
+                                sideTwo.innerHTML = '<input id = "Two" type = "number" >';
+                                sideThree.innerHTML = '<input id = "Three" type = "number" >';
+                                sideFour.innerHTML = '<input id = "Four" type = "number" >';
+                                sideFive.innerHTML = '<input id = "Five" type = "number" >';
+                                sideSix.innerHTML = '<input id = "Six" type = "number" >';
+                                sideSeven.innerHTML = '<div id = "Seven"> </div>';
+                                sideEight.innerHTML = '<div id = "Eight"> </div>';
+                                sideNine.innerHTML = '<div id = "Nine"> </div>';
+                                sideTen.innerHTML = '<div id = "Ten"> </div>';
+
+                                theButton.innerHTML = '<button type = "button" onclick = "typeOfFigure()" >Resultado</button>';
+                                theFigure.innerHTML = "";
+                                thePerimeter.innerHTML = "";
+                break;
+
+                case "7": 
+                                sideOne.innerHTML = '<input id = "One" type = "number" >';
+                                sideTwo.innerHTML = '<input id = "Two" type = "number" >';
+                                sideThree.innerHTML = '<input id = "Three" type = "number" >';
+                                sideFour.innerHTML = '<input id = "Four" type = "number" >';
+                                sideFive.innerHTML = '<input id = "Five" type = "number" >';
+                                sideSix.innerHTML = '<input id = "Six" type = "number" >';
+                                sideSeven.innerHTML = '<input id = "Seven" type = "number" >';
+                                sideEight.innerHTML = '<div id = "Eight"> </div>';
+                                sideNine.innerHTML = '<div id = "Nine"> </div>';
+                                sideTen.innerHTML = '<div id = "Ten"> </div>';
+
+                                theButton.innerHTML = '<button type = "button" onclick = "typeOfFigure()" >Resultado</button>';
+                                theFigure.innerHTML = "";
+                                thePerimeter.innerHTML = "";
+                break;
+
+                case "8": 
+                                sideOne.innerHTML = '<input id = "One" type = "number" >';
+                                sideTwo.innerHTML = '<input id = "Two" type = "number" >';
+                                sideThree.innerHTML = '<input id = "Three" type = "number" >';
+                                sideFour.innerHTML = '<input id = "Four" type = "number" >';
+                                sideFive.innerHTML = '<input id = "Five" type = "number" >';
+                                sideSix.innerHTML = '<input id = "Six" type = "number" >';
+                                sideSeven.innerHTML = '<input id = "Seven" type = "number" >';
+                                sideEight.innerHTML = '<input id = "Eight" type = "number" >';
+                                sideNine.innerHTML = '<div id = "Nine"> </div>';
+                                sideTen.innerHTML = '<div id = "Ten"> </div>';
+
+                                theButton.innerHTML = '<button type = "button" onclick = "typeOfFigure()" >Resultado</button>';
+                                theFigure.innerHTML = "";
+                                thePerimeter.innerHTML = "";
+                break;
+
+                case "9": 
+                                sideOne.innerHTML = '<input id = "One" type = "number" >';
+                                sideTwo.innerHTML = '<input id = "Two" type = "number" >';
+                                sideThree.innerHTML = '<input id = "Three" type = "number" >';
+                                sideFour.innerHTML = '<input id = "Four" type = "number" >';
+                                sideFive.innerHTML = '<input id = "Five" type = "number" >';
+                                sideSix.innerHTML = '<input id = "Six" type = "number" >';
+                                sideSeven.innerHTML = '<input id = "Seven" type = "number" >';
+                                sideEight.innerHTML = '<input id = "Eight" type = "number" >';
+                                sideNine.innerHTML = '<input id = "Nine" type = "number" >';
+                                sideTen.innerHTML = '<div id = "Ten"> </div>';
+
+                                theButton.innerHTML = '<button type = "button" onclick = "typeOfFigure()" >Resultado</button>';
+                                theFigure.innerHTML = "";
+                                thePerimeter.innerHTML = "";
+                break;
+
+                case "10": 
+                                sideOne.innerHTML = '<input id = "One" type = "number" >';
+                                sideTwo.innerHTML = '<input id = "Two" type = "number" >';
+                                sideThree.innerHTML = '<input id = "Three" type = "number" >';
+                                sideFour.innerHTML = '<input id = "Four" type = "number" >';
+                                sideFive.innerHTML = '<input id = "Five" type = "number" >';
+                                sideSix.innerHTML = '<input id = "Six" type = "number" >';
+                                sideSeven.innerHTML = '<input id = "Seven" type = "number" >';
+                                sideEight.innerHTML = '<input id = "Eight" type = "number" >';
+                                sideNine.innerHTML = '<input id = "Nine" type = "number" >';
+                                sideTen.innerHTML = '<input id = "Ten" type = "number" >';
+
+                                theButton.innerHTML = '<button type = "button" onclick = "typeOfFigure()" >Resultado</button>';
+                                theFigure.innerHTML = "";
+                                thePerimeter.innerHTML = "";
+                break;
+
+                default:
+                        sideOne.innerHTML = "";
+                        sideTwo.innerHTML = "";
+                        sideThree.innerHTML = "";
+                        sideFour.innerHTML = "";
+                        sideFive.innerHTML = "";
+                        sideSix.innerHTML = "";
+                        sideSeven.innerHTML = "";
+                        sideEight.innerHTML = "";
+                        sideNine.innerHTML = "";
+                        sideTen.innerHTML = "";
+                break;
+        }
+
+}
+
+function typeOfFigure()
+{
+        var InputValue = document.getElementById("sides").value;
+
+        theFigure = document.getElementById("showFigure");
+        thePerimeter = document.getElementById("showPerimeter");
+
+        let perimeter;
+
+        sideOne = document.getElementById("One").value;
+        sideTwo = document.getElementById("Two").value;
+        sideThree = document.getElementById("Three").value;
+        sideFour = document.getElementById("Four").value;
+        sideFive = document.getElementById("Five").value;
+        sideSix = document.getElementById("Six").value;
+        sideSeven = document.getElementById("Seven").value;
+        sideEight = document.getElementById("Eight").value;
+        sideNine = document.getElementById("Nine").value;
+        sideTen = document.getElementById("Ten").value;
+
+        operation = 0;
+
+        switch (InputValue){
+
+                case "3":
+                        operation = parseFloat(sideOne) + parseFloat(sideTwo) + parseFloat(sideThree);
+                        theFigure.innerHTML = '<b> La figura es un triangulo </b>';
+                        perimeter = "Su perimetro es: " + operation;
+                        thePerimeter.innerHTML = perimeter;
+                break;
+
+                case "4":
+                        operation = parseFloat(sideOne) + parseFloat(sideTwo) + parseFloat(sideThree) + parseFloat(sideFour);
+                        theFigure.innerHTML = '<b> La figura es un cuadrado </b>';
+                        perimeter = "Su perimetro es: " + operation;
+                        thePerimeter.innerHTML = perimeter;
+                break;
+
+                case "5":
+                        operation = parseFloat(sideOne) + parseFloat(sideTwo) + parseFloat(sideThree) + parseFloat(sideFour) + parseFloat(sideFive);
+                        theFigure.innerHTML = '<b> La figura es un pentágono </b>';
+                        perimeter = "Su perimetro es: " + operation;
+                        thePerimeter.innerHTML = perimeter;
+                break;
+
+                case "6":
+                        operation = parseFloat(sideOne) + parseFloat(sideTwo) + parseFloat(sideThree) + parseFloat(sideFour) + parseFloat(sideFive)
+                        + parseFloat (sideSix);
+                        theFigure.innerHTML = '<b> La figura es un hexágono </b>';
+                        perimeter = "Su perimetro es: " + operation;
+                        thePerimeter.innerHTML = perimeter;
+                break;
+
+                case "7":
+                        operation = parseFloat(sideOne) + parseFloat(sideTwo) + parseFloat(sideThree) + parseFloat(sideFour) + parseFloat(sideFive)
+                        + parseFloat(sideSix) + parseFloat(sideSeven);
+                        theFigure.innerHTML = '<b> La figura es un heptágono </b>';
+                        perimeter = "Su perimetro es: " + operation;
+                        thePerimeter.innerHTML = perimeter;
+                break;
+
+                case "8":
+                        operation = parseFloat(sideOne) + parseFloat(sideTwo) + parseFloat(sideThree) + parseFloat(sideFour) + parseFloat(sideFive)
+                        + parseFloat(sideSix) + parseFloat(sideSeven) + parseFloat(sideEight);
+                        theFigure.innerHTML = '<b> La figura es un octágono </b>';
+                        perimeter = "Su perimetro es: " + operation;
+                        thePerimeter.innerHTML = perimeter;
+                break;
+
+                case "9":
+                        operation = parseFloat(sideOne) + parseFloat(sideTwo) + parseFloat(sideThree) + parseFloat(sideFour) + parseFloat(sideFive)
+                        + parseFloat(sideSix) + parseFloat(sideSeven) + parseFloat(sideEight) + parseFloat(sideNine);
+                        theFigure.innerHTML = '<b> La figura es un eneágono </b>';
+                        perimeter = "Su perimetro es: " + operation;
+                        thePerimeter.innerHTML = perimeter;
+                break;
+
+                case "10":
+                        operation = parseFloat(sideOne) + parseFloat(sideTwo) + parseFloat(sideThree) + parseFloat(sideFour) + parseFloat(sideFive)
+                        + parseFloat(sideSix) + parseFloat(sideSeven) + parseFloat(sideEight) + parseFloat(sideNine) + parseFloat(sideTen);
+                        theFigure.innerHTML = '<b> La figura es un decágono </b>';
+                        perimeter = "Su perimetro es: " + operation;
+                        thePerimeter.innerHTML = perimeter;
+                break;
+        }
+}
