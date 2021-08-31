6fe5d7885c0dfc7c17e94c198d108c5964fa6bc8
Fernando747 <fernandojcr5@gmail.com>
2021-08-31 08:40:55 -0500

    Tarea actualizada

--- Tarea 1/main.js
+++ Tarea 1/main.js
@@ -1,4 +1,7 @@
 function Identify(){
+  var shapes = ['Digon','Trígono','Tetrágono','Pentágono','Hexágono','Octágono','Nonágono','Decágono',
+  'Undecágono','Dodecágono','Tridecágono','Tetradecágono','Pentadecágono','Hexadecágono','Heptadecágono','Octadecágono','Eneadecágono','Icoságono']
+
     let input_text = document.getElementById("Input").value;
     var number_array = input_text.split(',');
     var answer = 0;
@@ -11,68 +14,6 @@ function Identify(){
     document.getElementById('Perimeter').innerHTML=answer;
     document.getElementById('Sides').innerHTML=number_array.length;
 
-    switch(number_array.length) {
-        case 2:
-            document.getElementById('Shape').innerHTML= "Digon";
-          break;
-        case 3:
-            document.getElementById('Shape').innerHTML= "Trígono";
-          break;
-          case 4:
-            document.getElementById('Shape').innerHTML= "Tetrágono";
-          break;
-          case 5:
-            document.getElementById('Shape').innerHTML= "Pentágono";
-          break;
-          case 6:
-            document.getElementById('Shape').innerHTML= "Hexágono";
-          break;
-          case 7:
-            document.getElementById('Shape').innerHTML= "Heptágono";
-          break;
-          case 8:
-            document.getElementById('Shape').innerHTML= "Octágono";
-          break;
-          case 9:
-            document.getElementById('Shape').innerHTML= "Nonágono";
-          break;
-          case 10:
-            document.getElementById('Shape').innerHTML= "Decágono";
-          break;
-          case 11:
-            document.getElementById('Shape').innerHTML= "Undecágono";
-          break;
-          case 12:
-            document.getElementById('Shape').innerHTML= "Dodedecágono";
-          break;
-          case 13:
-            document.getElementById('Shape').innerHTML= "Tridecágono";
-          break;
-          case 14:
-            document.getElementById('Shape').innerHTML= "Tetradecágono";
-          break;
-          case 15:
-            document.getElementById('Shape').innerHTML= "Pentadecágono";
-          break;
-          case 16:
-            document.getElementById('Shape').innerHTML= "Hexadecágono";
-          break;
-          case 17:
-            document.getElementById('Shape').innerHTML= "Heptadecágono";
-          break;
-          case 18:
-            document.getElementById('Shape').innerHTML= "Octadecágono";
-          break;
-          case 19:
-            document.getElementById('Shape').innerHTML= "Eneadecágono";
-          break;
-          case 20:
-            document.getElementById('Shape').innerHTML= "Icoságono";
-          break;
-        default:
-            document.getElementById('Shape').innerHTML= "Figura no reconocida (2-20 lados)";
-            alert(number_array.length);
-            break;
-      }
-
+   document.getElementById('Shape').innerHTML = shapes[number_array.length -2];
+    
 }
