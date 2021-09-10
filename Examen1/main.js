96ffd595ad1e09a7e457e1399dd64c2c76e98731
Fernando747 <fernandojcr5@gmail.com>
2021-09-09 22:23:26 -0500

    Se restablece main

--- Examen1/main.js
+++
@@ -1,9 +0,0 @@
-function cube(){
-
-var input_value = document.getElementById("number").value;
-    var answer = input_value * input_value * input_value;
-
-    let myH1 = document.createElement("h1");
-    myH1.innerText = answer;
-    document.getElementById("mydiv").append(myH1);
-}
