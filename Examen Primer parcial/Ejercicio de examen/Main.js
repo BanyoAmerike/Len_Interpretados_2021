17c23d5bb97b36036b99c7258554b70d3865f797
Angel Legorreta <legosurio@hotmail.com>
2021-09-12 16:39:01 -0500

    Fixing my Git

---
+++ Examen Primer parcial/Ejercicio de examen/Main.js
@@ -0,0 +1,11 @@
+
+let input_value = document.getElementById("numero").value
+let resultado = input_value * (input_value * input_value);
+
+function cubo () {
+    resultado.innerText = ver;
+    document.body.append(resultado);
+    alert(ver);
+    
+}
+
