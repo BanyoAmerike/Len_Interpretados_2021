3eaffd27771d75da540c74354bd9604b8b966243
Daniel <daniel88tb@gmail.com>
2021-10-06 14:12:15 -0500

    3er semestre completo awebo que si
    Yaaaaaaa estuvo y le entendi bien chingon profe <3

--- Ejercicio 3/primersitio/Home/static/js/leap_main.js
+++ Ejercicio 3/primersitio/Home/static/js/leap_main.js
@@ -2,22 +2,18 @@
 
 function leap()
 {
-    let inpput_value = document.getElementById("year").value;
-    let ver = inpput_value % 4;
-    if (ver == 0)
-    {
-        alert("El año " + inpput_value + "es biciesto :D");
-    }
-}
-function charValidation(event)
-{
-    var key = parseInt(event.key);
     let input_value = document.getElementById("year").value;
-
-    if (Number.isNaN(key))
-    {
-        let new_value = input_value.replae(event.key, "");
-        document.getElementById("year").value = new_value;
-        console.log(typeof input_value);
+    if (input_value%4 == 0){
+        if(input_value%100 == 0){
+            if(input_value%400 == 0){
+                alert("Es bisiesto");
+            } else {
+                alert ("No es bisiesto");
+            }
+        } else {
+            alert ("Es bisiesto");
+        }       
+    } else{
+        alert ("No es bisiesto");
     }
 }
