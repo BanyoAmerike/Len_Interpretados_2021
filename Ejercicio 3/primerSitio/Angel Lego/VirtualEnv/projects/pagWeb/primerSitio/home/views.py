478982a84b99b139c84908d52e94b547d726c23f
Angel Legorreta <legosurio@hotmail.com>
2021-10-06 15:44:12 -0500

    El experimento no jaló :'v

---
+++ Ejercicio 3/primerSitio/Angel Lego/VirtualEnv/projects/pagWeb/primerSitio/home/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def primerView(request):
+    return HttpResponse('Hola desde el servidor')
+
+def leap_year(request):
+    return render(request, 'index.html')
