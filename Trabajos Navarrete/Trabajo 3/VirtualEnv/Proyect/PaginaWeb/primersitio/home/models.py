730a5f44b6b422ceee3433376908b09f33ba3dc9
Daniel Navarrete <daniel_navarrete_05@hotmail.com>
2021-10-18 23:34:24 -0500

    3er semestre
    olvide hacer el commit Xd

--- Trabajos Navarrete/Trabajo 3/VirtualEnv/Proyect/PaginaWeb/primersitio/home/models.py
+++ Trabajos Navarrete/Trabajo 3/VirtualEnv/Proyect/PaginaWeb/primersitio/home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Leap_years(models.Model):
+    year = models.IntegerField()
+    date = models.DateField()
+    hour = models.TextField()
