95b188afc0f7e8ad8d5200ceb894e4ab9d0f503c
banyo <banyoloper@gmail.com>
2021-10-20 09:00:19 -0500

    Se agregan archivos del ejercicio 5

--- Ejercicio5/login/views.py
+++ Ejercicio5/login/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def login(request):
+    return render(request, 'login.html' )
+
+def login_request(request):
+    user = request.POST['user']
+    password = request.POST['pass']
+    '''
+    if(user == user de base de datos and password == pass base de datos):
+        HttpResponse(f'bienvenido {user}')
+    else:
+        HttpResponse('no te reconozco')
+    '''
+    return HttpResponse(f'Este es el usuario que se manda desde el login: {user}')
